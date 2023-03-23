45019d808102b55d3bb02c3ed56625e4f6a542d0
SuperMax <euclido@live.com>
2023-03-23 09:43:30 -0500

    working the header nav

---
+++ src/template/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from "../components/header/Header";
+import Banner from "../components/sections/banner_v";
+
+export default function home() {
+  return (
+    <>
+      <Header/>
+      <Banner/>
+    </>
+  )
+}
